01dd3fe8daee7c3a470a5c9072feb0347c8791b8
Joshvdw <joshvdw@live.com>
2025-07-11 16:56:17 +0200

    layed down styling foundations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from "next";
-import { Lexend } from "next/font/google";
+import type {Metadata} from "next";
+import {Lexend} from "next/font/google";
 import "./globals.css";
 
 const lexendSans = Lexend({
-  variable: "--font-lexend-sans",
-  subsets: ["latin"],
+    variable: "--font-lexend-sans",
+    subsets: ["latin"],
+    display: "swap",
 });
 
+
 export const metadata: Metadata = {
-  title: "Davey Jones' Deckhand",
-  description: "Perfect. Pirate. Parries. Unleash outrageous counter-attack chains in this swashbuckling adventure. Will you break free from Davy Jones' locker?\n",
+    title: "Davey Jones' Deckhand",
+    description: "Perfect. Pirate. Parries. Unleash outrageous counter-attack chains in this swashbuckling adventure. Will you break free from Davy Jones' locker?\n",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${lexendSans.variable}`}>
+    return (
+        <html lang="en" className={`${lexendSans.variable}`}>
+        <body>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
