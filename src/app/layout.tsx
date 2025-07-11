c21cbe4f1e2f295ee0a2dbcd54d84faf3298b44b
Joshvdw <joshvdw@live.com>
2025-07-11 10:47:02 +0200

    added analytics and page speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from "next";
-import { Lexend } from "next/font/google";
+import type {Metadata} from "next";
+import {Analytics} from "@vercel/analytics/next"
+import {SpeedInsights} from '@vercel/speed-insights/next';
+import {Lexend} from "next/font/google";
 import "./globals.css";
 
 const lexendSans = Lexend({
-  variable: "--font-lexend-sans",
-  subsets: ["latin"],
+    variable: "--font-lexend-sans",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Davey Jones' Deckhand",
-  description: "Perfect. Pirate. Parries. Unleash outrageous counter-attack chains in this swashbuckling adventure. Will you break free from Davy Jones' locker?\n",
+    title: "Davey Jones' Deckhand",
+    description: "Perfect. Pirate. Parries. Unleash outrageous counter-attack chains in this swashbuckling adventure. Will you break free from Davy Jones' locker?\n",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+export default function RootLayout({children,}: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${lexendSans.variable}`}>
+    return (
+        <html lang="en">
+        <body className={`${lexendSans.variable}`}>
         {children}
-      </body>
-    </html>
-  );
+        <Analytics/>
+        <SpeedInsights/>
+        </body>
+        </html>
+    );
 }
